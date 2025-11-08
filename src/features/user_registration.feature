a90574627e77c5b8f16b1b35b03de02bbc2cc26f
Sergio E Bautista M <sebautistam@gmail.com>
2025-11-08 10:32:31 -0500

    Completed user registration feature

--- src/features/user_registration.feature
+++ src/features/user_registration.feature
@@ -6,7 +6,7 @@ Feature: User registration
     Scenario: Registration with incorrect information
         Given an already registered user <firstname> <lastname>
         When I enter <firstname> <lastname> <email> <password> in the registration form
-        Then I see an error message
+        Then I see a "registration" error message with text "A customer with this email address already exists."
         And the account is not created
 
 Examples:
@@ -15,4 +15,13 @@ Examples:
 | Jane      | Doe      | customer@practicesoftwaretesting.com  | Welcome01. |
 | Jack      | Howe     | customer2@practicesoftwaretesting.com | Welcome01. |
 | Bob       | Smith    | customer3@practicesoftwaretesting.com | Pass123.   |
+
+    Scenario: Registration with incorrect information
+        When I fill out the registration form without email address
+        Then I see a "email" error message with text "Email is required"
+        And the account is not created
+
+    Scenario: Successful registration on the website
+		When I fill out the registration form correctly
+		Then the account is created
 
--- src/step-definitions/user_registration-steps.js
+++ src/step-definitions/user_registration-steps.js
@@ -1,7 +1,7 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 const { expect, $ } = require('@wdio/globals')
 
-Given('I am on the registration page', async function () {
+Given(/^I am on the registration page$/, async function () {
     // Navigate to the registration page
     await browser.url('/auth/register');
 });
@@ -29,12 +29,69 @@ When(/^I enter (.+) (.+) (.+) (.+) in the registration form$/,
         await $('button[type="submit"]').click();
     });
 
-Then('I see an error message', async function () {
-    const error = await $('[data-test="register-error"]');
+Then(/^I see a "([^"]*)" error message with text "([^"]*)"$/, async function (errorType, expectedText) {
+    const selectorsError = {
+        'email': '[data-test="email-error"]',
+        'registration': '[data-test="register-error"]'
+    }
+    const error = await $(selectorsError[errorType]);
     await expect(error).toBeDisplayed();
+    await expect(await error.getText()).toContain(expectedText);
 });
 
-Then('the account is not created', async function () {
+Then(/^the account is not created$/, async function () {
     const url = await browser.getUrl();
     await expect(url).toContain('auth/register');
 });
+
+When(/^I fill out the registration form without email address$/, async function() {
+    await $('#first_name').setValue('Alan');
+    await $('#last_name').setValue('Smith');
+
+    await $('#dob').setValue('1999-01-02');
+    await $('#street').setValue('123 Road Street');
+    await $('#postal_code').setValue('12345');
+    await $('#city').setValue('NYC');
+    await $('#state').setValue('New York');
+    await $('#country').selectByAttribute('value', 'US');
+    await $('#phone').setValue('123456789');
+
+    await $('#email').setValue('');
+    await $('#password').setValue('Welcome01.');
+
+    await $('button[type="submit"]').click();
+});
+
+When(/^I fill out the registration form correctly$/, async function () {
+    
+    await $('#first_name').setValue('Alan');
+    await $('#last_name').setValue('Smith');
+
+    await $('#dob').setValue('1999-01-02');
+    await $('#street').setValue('123 Road Street');
+    await $('#postal_code').setValue('12345');
+    await $('#city').setValue('NYC');
+    await $('#state').setValue('New York');
+    await $('#country').selectByAttribute('value', 'US');
+    await $('#phone').setValue('123456789');
+
+    const timestamp = Date.now();
+    uniqueEmail = `alan.smith${timestamp}@test.com`
+    await $('#email').setValue(uniqueEmail);
+    await $('#password').setValue('Yjkasd2oasdww.');
+
+    await $('button[type="submit"]').click();
+});
+
+Then (/^the account is created$/, async function () {
+    await browser.waitUntil(
+        async () => (await browser.getUrl()).includes('auth/login'),
+        {
+            timeout: 5000,
+            timeoutMsg: 'Expected to be redirected to login page within 5s'
+        }
+    );
+});
+
+
+
